c9853c655fcdccca283611a04498729eef2d47ba
igor <igor.strupinskiy@gmail.com>
2017-08-30 21:38:26 -0700

    initial commit

---
+++ src/Components/Countdown/CountdownList/Countdown_List_Container.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+
+class CountdownListContainer extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            countdowns: []
+        };
+    }
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default CountdownListContainer;
