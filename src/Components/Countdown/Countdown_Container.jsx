539b1d7f528b4a6a6a207e957644175ffdb23457
Strupinskiy <IStrupin@russellinvestments.com>
2017-08-31 11:31:01 -0700

    create dummy classes

---
+++ src/Components/Countdown/Countdown_Container.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+
+class CountdownContainer extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            targetDatetime: []
+        };
+    }
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default CountdownContainer;
