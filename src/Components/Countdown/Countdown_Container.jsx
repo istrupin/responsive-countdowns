fb63051458fdad2cdf249f370ac719fefa1b72be
Strupinskiy <IStrupin@russellinvestments.com>
2017-09-06 08:30:28 -0700

    basic working prototype of countdown container

--- src/Components/Countdown/Countdown_Container.jsx
+++ src/Components/Countdown/Countdown_Container.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Countdown from './Countdown';
 
 
 class CountdownContainer extends Component {
@@ -7,18 +8,42 @@ class CountdownContainer extends Component {
 
         this.state = {
             targetDatetime: [],
-            secondsLeft: 0
+            secondsLeft: 5
         };
+
+
+        this.countDown = this.countDown.bind(this);
+        this.startTimer = this.startTimer.bind(this);
+        
+    }
+
+    componentDidMount(){
+        this.startTimer();
+    }
+
+    componentDidUpdate(){
+        this.startTimer();
+        console.log('mnt');
+    }
+
+    startTimer() {
+        if(this.state.secondsLeft > 0) {
+            setTimeout(this.countDown, 1000);
+        }
+
     }
 
     countDown(){
-
+        console.log('called');
+        let { secondsLeft } = this.state; 
+        secondsLeft--;
+        this.setState({secondsLeft: secondsLeft});
     }
 
     render() {
         return (
             <div>
-                
+                <Countdown secondsLeft={this.state.secondsLeft} > </Countdown>
             </div>
         )
     }
